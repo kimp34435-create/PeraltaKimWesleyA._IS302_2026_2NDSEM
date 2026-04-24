62933291ba8f9c191d4fa0adc80f16111b264d82
Kim_Peralta <kimp34435@gmail.com>
2026-04-24 11:22:08 +0800

    Update secure_inventory.py

--- Kim Peralta/week06_encapsulation/secure_inventory.py
+++ Kim Peralta/week06_encapsulation/secure_inventory.py
@@ -1,22 +1,23 @@
-class Product_eas:
-    def __init__(self_eas, name_eas, price_eas, quantity_eas):
-        self_eas.__name = name_eas
-        self_eas.__price = price_eas
-        self_eas.__quantity = quantity_eas
+class Product_kwap:
+    def __init__(self_kwap, name_kwap, price_kwap, quantity_kwap):
+        self_kwap.__name = name_kwap
+        self_kwap.__price = price_kwap
+        self_kwap.__quantity = quantity_kwap
 
-    def get_product_info_eas(self_eas):
-        print("Product:", self_eas.__name)
-        print("Price:", self_eas.__price)
-        print("Quantity:", self_eas.__quantity)
+    def get_product_info_kwap(self_kwap):
+        print("Product:", self_kwap.__name)
+        print("Price:", self_kwap.__price)
+        print("Quantity:", self_kwap.__quantity)
 
-    def update_quantity_eas(self_eas, new_quantity_eas):
-        if new_quantity_eas >= 0:
-            self_eas.__quantity = new_quantity_eas
+    def update_quantity_kwap(self_kwap, new_quantity_kwap):
+        if new_quantity_kwap >= 0:
+            self_kwap.__quantity = new_quantity_kwap
 
-    def update_price_eas(self_eas, new_price_eas):
-        if new_price_eas > 0:
-            self_eas.__price = new_price_eas
+    def update_price_kwap(self_kwap, new_price_kwap):
+        if new_price_kwap > 0:
+            self_kwap.__price = new_price_kwap
+
 
 # Example usage
-product_eas = Product_eas("Laptop", 45000, 10)
-product_eas.get_product_info_eas()
+product_kwap = Product_kwap("Laptop", 45000, 10)
+product_kwap.get_product_info_kwap()
