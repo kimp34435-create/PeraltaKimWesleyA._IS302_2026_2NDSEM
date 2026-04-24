20c3c81f7360d9a72bb0b53a2fafd8e478aaa2b2
Kim_Peralta <kimp34435@gmail.com>
2026-04-24 11:08:59 +0800

    Update calculator_functions.py

--- Kim Peralta/Week03_Act/calculator_functions.py
+++ Kim Peralta/Week03_Act/calculator_functions.py
@@ -1,7 +1,7 @@
-def add_numbers(a_eas, b_eas):
-    return a_eas + b_eas
+def add_numbers(a_kwap, b_kwap):
+    return a_kwap + b_kwap
 
-num1_eas = float(input("Enter first number: "))
-num2_eas = float(input("Enter second number: "))
-result_eas = add_numbers(num1_eas, num2_eas)
-print("Sum:", result_eas)
+num1_kwap = float(input("Enter first number: "))
+num2_kwap = float(input("Enter second number: "))
+result_kwap = add_numbers(num1_kwap, num2_kwap)
+print("Sum:", result_kwap)
