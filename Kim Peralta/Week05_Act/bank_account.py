e516ef724603ec12c906edfb2d462df8477cf48c
Kim_Peralta <kimp34435@gmail.com>
2026-04-24 11:16:36 +0800

    Update bank_account.py

--- Kim Peralta/Week05_Act/bank_account.py
+++ Kim Peralta/Week05_Act/bank_account.py
@@ -1,24 +1,24 @@
-class BankAccount_eas:
-    def __init__(self_eas, account_name_eas, balance_eas):
-        self_eas.account_name_eas = account_name_eas
-        self_eas.balance_eas = balance_eas
+class BankAccount_kwap:
+    def __init__(self_kwap, account_name_kwap, balance_kwap):
+        self_kwap.account_name_kwap = account_name_kwap
+        self_kwap.balance_kwap = balance_kwap
     
-    def deposit_eas(self_eas, amount_eas):
-        self_eas.balance_eas += amount_eas
+    def deposit_kwap(self_kwap, amount_kwap):
+        self_kwap.balance_kwap += amount_kwap
         print("Deposit successful")
     
-    def withdraw_eas(self_eas, amount_eas):
-        if amount_eas <= self_eas.balance_eas:
-            self_eas.balance_eas -= amount_eas
+    def withdraw_kwap(self_kwap, amount_kwap):
+        if amount_kwap <= self_kwap.balance_kwap:
+            self_kwap.balance_kwap -= amount_kwap
             print("Withdrawal successful")
         else:
             print("Insufficient balance")
     
-    def display_balance_eas(self_eas):
-        print("Balance:", self_eas.balance_eas)
+    def display_balance_kwap(self_kwap):
+        print("Balance:", self_kwap.balance_kwap)
 
 
-account_eas = BankAccount_eas("Juan", 5000)
-account_eas.deposit_eas(1000)
-account_eas.withdraw_eas(2000)
-account_eas.display_balance_eas()
+account_kwap = BankAccount_kwap("Juan", 5000)
+account_kwap.deposit_kwap(1000)
+account_kwap.withdraw_kwap(2000)
+account_kwap.display_balance_kwap()
