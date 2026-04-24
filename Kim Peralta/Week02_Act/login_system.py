ec0ebc7dd160794ed7b369d76e16df6fdf815ddf
Kim_Peralta <kimp34435@gmail.com>
2026-04-24 11:06:15 +0800

    Update login_system.py

--- Kim Peralta/Week02_Act/login_system.py
+++ Kim Peralta/Week02_Act/login_system.py
@@ -1,14 +1,17 @@
-correct_username_eas = "admin"
-correct_password_eas = "1234"
-attempts_eas = 0
-while attempts_eas < 3:
-    username_eas = input("Enter username: ")
-    password_eas = input("Enter password: ")
-    if username_eas == correct_username_eas and password_eas == correct_password_eas:
+correct_username_kwap = "admin"
+correct_password_kwap = "1234"
+attempts_kwap = 0
+
+while attempts_kwap < 3:
+    username_kwap = input("Enter username: ")
+    password_kwap = input("Enter password: ")
+    
+    if username_kwap == correct_username_kwap and password_kwap == correct_password_kwap:
         print("Login Successful")
         break
     else:
         print("Invalid Credentials")
-        attempts_eas += 1
-if attempts_eas == 3:
+        attempts_kwap += 1
+
+if attempts_kwap == 3:
     print("Account Locked")
