950333fc666b661fe2ac21929b57d6db8ded3fa6
Kim_Peralta <kimp34435@gmail.com>
2026-04-24 11:25:14 +0800

    Update student_inheritance.py

--- Kim Peralta/week07_inheritance/student_inheritance.py
+++ Kim Peralta/week07_inheritance/student_inheritance.py
@@ -1,19 +1,19 @@
-class Person_eas:
-    def __init__(self_eas, name_eas, age_eas):
-        self_eas.name_eas = name_eas
-        self_eas.age_eas = age_eas
+class Person_kwap:
+    def __init__(self_kwap, name_kwap, age_kwap):
+        self_kwap.name_kwap = name_kwap
+        self_kwap.age_kwap = age_kwap
 
 
-class Student_eas(Person_eas):
-    def __init__(self_eas, name_eas, age_eas, course_eas):
-        super().__init__(name_eas, age_eas)
-        self_eas.course_eas = course_eas
+class Student_kwap(Person_kwap):
+    def __init__(self_kwap, name_kwap, age_kwap, course_kwap):
+        super().__init__(name_kwap, age_kwap)
+        self_kwap.course_kwap = course_kwap
 
-    def display_student_eas(self_eas):
-        print("Name:", self_eas.name_eas)
-        print("Age:", self_eas.age_eas)
-        print("Course:", self_eas.course_eas)
+    def display_student_kwap(self_kwap):
+        print("Name:", self_kwap.name_kwap)
+        print("Age:", self_kwap.age_kwap)
+        print("Course:", self_kwap.course_kwap)
 
 
-student1_eas = Student_eas("Maria", 20, "BSIS")
-student1_eas.display_student_eas()
+student1_kwap = Student_kwap("Maria", 20, "BSIS")
+student1_kwap.display_student_kwap()
