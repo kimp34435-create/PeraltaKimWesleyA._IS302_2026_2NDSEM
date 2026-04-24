fc725b45c455213ddee488513ea1b1a24c4dfcb3
Kim_Peralta <kimp34435@gmail.com>
2026-04-24 11:25:43 +0800

    Update vehicle_system.py

--- Kim Peralta/week07_inheritance/vehicle_system.py
+++ Kim Peralta/week07_inheritance/vehicle_system.py
@@ -1,17 +1,17 @@
-class Vehicle_eas:
-    def __init__(self_eas, brand_eas, model_eas):
-        self_eas.brand_eas = brand_eas
-        self_eas.model_eas = model_eas
+class Vehicle_kwap:
+    def __init__(self_kwap, brand_kwap, model_kwap):
+        self_kwap.brand_kwap = brand_kwap
+        self_kwap.model_kwap = model_kwap
 
 
-class Car_eas(Vehicle_eas):
-    def __init__(self_eas, brand_eas, model_eas, year_eas):
-        super().__init__(brand_eas, model_eas)
-        self_eas.year_eas = year_eas
+class Car_kwap(Vehicle_kwap):
+    def __init__(self_kwap, brand_kwap, model_kwap, year_kwap):
+        super().__init__(brand_kwap, model_kwap)
+        self_kwap.year_kwap = year_kwap
 
-    def display_car_eas(self_eas):
-        print(self_eas.brand_eas, self_eas.model_eas, self_eas.year_eas)
+    def display_car_kwap(self_kwap):
+        print(self_kwap.brand_kwap, self_kwap.model_kwap, self_kwap.year_kwap)
 
 
-car1_eas = Car_eas("Toyota", "Corolla", 2022)
-car1_eas.display_car_eas()
+car1_kwap = Car_kwap("Toyota", "Corolla", 2022)
+car1_kwap.display_car_kwap()
