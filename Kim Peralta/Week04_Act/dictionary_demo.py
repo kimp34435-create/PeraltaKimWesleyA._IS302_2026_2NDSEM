7291cfe9fa4099f0553f9d66731f4f6a3d760ece
Kim_Peralta <kimp34435@gmail.com>
2026-04-24 11:12:58 +0800

    Rename student_eas to student_kwap in dictionary demo

--- Kim Peralta/Week04_Act/dictionary_demo.py
+++ Kim Peralta/Week04_Act/dictionary_demo.py
@@ -1,8 +1,8 @@
-student_eas = {
+student_kwap = {
     "name": "Maria Santos",
     "age": 20,
     "course": "BSIS",
     "year_level": 2
 }
-for key_eas, value_eas in student_eas.items():
-    print(key_eas, ":", value_eas)
+for key_kwap, value_kwap in student_kwap.items():
+    print(key_kwap, ":", value_kwap)
