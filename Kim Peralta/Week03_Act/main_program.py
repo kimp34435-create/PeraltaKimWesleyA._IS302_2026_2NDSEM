6442063514da1bfeb17c42c1d80593026def5e63
Kim_Peralta <kimp34435@gmail.com>
2026-04-24 11:11:15 +0800

    Update main_program.py

--- Kim Peralta/Week03_Act/main_program.py
+++ Kim Peralta/Week03_Act/main_program.py
@@ -1,9 +1,9 @@
 import math_module
 
-num1_eas = float(input("Enter first number: "))
-num2_eas = float(input("Enter second number: "))
+num1_kwap = float(input("Enter first number: "))
+num2_kwap = float(input("Enter second number: "))
 
-print("Addition:", math_module.add(num1_eas, num2_eas))
-print("Subtraction:", math_module.subtract(num1_eas, num2_eas))
-print("Multiplication:", math_module.multiply(num1_eas, num2_eas))
-print("Division:", math_module.divide(num1_eas, num2_eas))
+print("Addition:", math_module.add(num1_kwap, num2_kwap))
+print("Subtraction:", math_module.subtract(num1_kwap, num2_kwap))
+print("Multiplication:", math_module.multiply(num1_kwap, num2_kwap))
+print("Division:", math_module.divide(num1_kwap, num2_kwap))
