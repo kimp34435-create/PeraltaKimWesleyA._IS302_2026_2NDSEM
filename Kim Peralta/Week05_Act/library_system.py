ad02ad6cd74e7c26562fc0c5400a05252872b087
Kim_Peralta <kimp34435@gmail.com>
2026-04-24 11:18:41 +0800

    Update library_system.py

--- Kim Peralta/Week05_Act/library_system.py
+++ Kim Peralta/Week05_Act/library_system.py
@@ -1,29 +1,29 @@
-class Book_eas:
-    def __init__(self_eas, title_eas, author_eas, year_eas):
-        self_eas.title_eas = title_eas
-        self_eas.author_eas = author_eas
-        self_eas.year_eas = year_eas
+class Book_kwap:
+    def __init__(self_kwap, title_kwap, author_kwap, year_kwap):
+        self_kwap.title_kwap = title_kwap
+        self_kwap.author_kwap = author_kwap
+        self_kwap.year_kwap = year_kwap
     
-    def display_book_eas(self_eas):
-        print("Title:", self_eas.title_eas)
-        print("Author:", self_eas.author_eas)
-        print("Year:", self_eas.year_eas)
+    def display_book_kwap(self_kwap):
+        print("Title:", self_kwap.title_kwap)
+        print("Author:", self_kwap.author_kwap)
+        print("Year:", self_kwap.year_kwap)
         print()
 
 
 print("----- LIBRARY MANAGEMENT SYSTEM -----\n")
 
 # Create three book objects
-book1_eas = Book_eas("Python Programming", "John Smith", 2022)
-book2_eas = Book_eas("Data Science Basics", "Sarah Johnson", 2021)
-book3_eas = Book_eas("Web Development Guide", "Mike Davis", 2023)
+book1_kwap = Book_kwap("Python Programming", "John Smith", 2022)
+book2_kwap = Book_kwap("Data Science Basics", "Sarah Johnson", 2021)
+book3_kwap = Book_kwap("Web Development Guide", "Mike Davis", 2023)
 
 # Display book information
 print("Book 1:")
-book1_eas.display_book_eas()
+book1_kwap.display_book_kwap()
 
 print("Book 2:")
-book2_eas.display_book_eas()
+book2_kwap.display_book_kwap()
 
 print("Book 3:")
-book3_eas.display_book_eas()
+book3_kwap.display_book_kwap()
