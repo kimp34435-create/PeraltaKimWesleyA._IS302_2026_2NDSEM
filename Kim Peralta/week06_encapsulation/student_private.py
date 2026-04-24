9e794c19d1c9746736a4759c36781f1c2bd56c8f
Kim_Peralta <kimp34435@gmail.com>
2026-04-24 11:22:40 +0800

    Update student_private.py

--- Kim Peralta/week06_encapsulation/student_private.py
+++ Kim Peralta/week06_encapsulation/student_private.py
@@ -1,14 +1,14 @@
-class Student_eas:
-    def __init__(self_eas, name_eas, student_id_eas, gpa_eas):
-        self_eas.__name = name_eas
-        self_eas.__student_id = student_id_eas
-        self_eas.__gpa = gpa_eas
+class Student_kwap:
+    def __init__(self_kwap, name_kwap, student_id_kwap, gpa_kwap):
+        self_kwap.__name = name_kwap
+        self_kwap.__student_id = student_id_kwap
+        self_kwap.__gpa = gpa_kwap
 
-    def get_student_info_eas(self_eas):
-        print("Name:", self_eas.__name)
-        print("Student ID:", self_eas.__student_id)
-        print("GPA:", self_eas.__gpa)
+    def get_student_info_kwap(self_kwap):
+        print("Name:", self_kwap.__name)
+        print("Student ID:", self_kwap.__student_id)
+        print("GPA:", self_kwap.__gpa)
 
 
-student1_eas = Student_eas("Juan", "2023-001", 1.75)
-student1_eas.get_student_info_eas()
+student1_kwap = Student_kwap("Juan", "2023-001", 1.75)
+student1_kwap.get_student_info_kwap()
