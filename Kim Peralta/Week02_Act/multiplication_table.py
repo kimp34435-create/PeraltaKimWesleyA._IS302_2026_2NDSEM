a1f5a506cc97c37f105fbf5ceb4552d0426abfc1
Kim_Peralta <kimp34435@gmail.com>
2026-04-24 11:07:08 +0800

    Update multiplication_table.py

--- Kim Peralta/Week02_Act/multiplication_table.py
+++ Kim Peralta/Week02_Act/multiplication_table.py
@@ -1,3 +1,3 @@
-number_eas = int(input("Enter a number: "))
-for i_eas in range(1, 11):
-    print(number_eas, "x", i_eas, "=", number_eas * i_eas)
+number_kwap = int(input("Enter a number: "))
+for i_kwap in range(1, 11):
+    print(number_kwap, "x", i_kwap, "=", number_kwap * i_kwap)
